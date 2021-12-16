08ba4182383e7329c3596bef486107d2de6924f7
Annalise Diroff <adiroff@hashicorp.com>
2021-12-16 11:20:07 -0800

    Fixing mssql docker issues

--- setup/database/populate.sh
+++ setup/database/populate.sh
@@ -2,8 +2,8 @@
 
 for i in {1..60};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SA_PASSWORD}" -d master /home/db-data/populate.sql
-    if [ $? -eq 0 ]
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SA_PASSWORD}" -d master -i /home/database/populate.sql
+    if [ $? -eq 1 ]
     then
         echo "database is populated & ready!"
         exit 0
