4e9fa65b48694d8563656c278b6cd9d5f0e8c13b
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2021-12-17 11:16:37 -0500

    Fix shellcheck warnings

--- setup/database/populate.sh
+++ setup/database/populate.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-for i in {1..60};
+for i in $(seq 1 60)
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SA_PASSWORD}" -d master /home/database/populate.sql
-    if [ $? -eq 0 ]
+    if /opt/mssql-tools/bin/sqlcmd \
+        -S localhost \
+        -U sa \
+        -P "${SA_PASSWORD}" \
+        -d master \
+        -i /home/database/populate.sql;
     then
         echo "database is populated & ready!"
         exit 0
     else
-        echo "waiting for database to be ready..."
+        echo "waiting for database to be ready... $i"
         sleep 1
     fi
 done
