82da6e4b9b7f8ac0b7886cf30a06e01aca08a874
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2021-12-21 12:06:42 -0500

    comments

--- app/Vault/VaultWrapperSettings.cs
+++ app/Vault/VaultWrapperSettings.cs
@@ -2,11 +2,14 @@ namespace app.Vault
 {
     public class VaultWrapperSettings
     {
+        // Vault server address
         public string Address { get; set; }
 
+        // AppRole credentials used to authenticate with Vault
         public string AppRoleAuthRoleId { get; set; }
         public string AppRoleAuthSecretIdFile { get; set; }
 
+        // Location and name of the api key secret within Vault
         public string ApiKeyPath { get; set; }
         public string ApiKeyDescriptor { get; set; }
     }
