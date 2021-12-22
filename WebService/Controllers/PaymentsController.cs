4c0c85b78f386fe986097356126bbad121f7f2b6
AnPucel <adiroff@hashicorp.com>
2021-12-22 12:05:27 -0800

    Added Logger & ReadMe Updates (#16)

--- WebService/Controllers/PaymentsController.cs
+++ WebService/Controllers/PaymentsController.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 using WebService.Vault;
 
@@ -12,10 +13,12 @@ namespace WebService.Controllers
     {
         private const string secureServiceEndpoint = "http://secure-service/api";
 
+        private readonly ILogger _logger;
         private VaultWrapper _vault;
 
-        public PaymentsController( VaultWrapper vault )
+        public PaymentsController(ILogger<PaymentsController> logger, VaultWrapper vault)
         {
+            _logger = logger;
             _vault = vault;
         }
 
@@ -24,17 +27,19 @@ namespace WebService.Controllers
         public string CreatePayment()
         {
             // fetch the secret api key from Vault
+            _logger.LogInformation("retrieving api key from Vault");
             string apiKey = _vault.GetSecretApiKey();
+            _logger.LogInformation("successfully retrieved api key from Vault");            
 
-            HttpWebRequest request = WebRequest.Create( secureServiceEndpoint ) as HttpWebRequest;
+            HttpWebRequest request = WebRequest.Create(secureServiceEndpoint) as HttpWebRequest;
 
             // add the secret api key to the request header
             request.Headers[ "x-api-key" ] = apiKey;
 
             // forward the response back to the caller
-            using ( HttpWebResponse response = request.GetResponse() as HttpWebResponse )
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
             {
-                using ( StreamReader sr = new StreamReader( response.GetResponseStream() ) )
+                using (StreamReader sr = new StreamReader( response.GetResponseStream()))
                 {
                     string stringResponse = sr.ReadToEnd();
                     return stringResponse;
