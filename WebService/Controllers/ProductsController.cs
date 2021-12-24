414a5b63b48d388b3c8d253fac9cb28f2dfe2fdd
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2021-12-23 19:54:50 -0500

    clean up imports

--- WebService/Controllers/ProductsController.cs
+++ WebService/Controllers/ProductsController.cs
@@ -1,8 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.IO;
-using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebService.DB;
