a9bdde928bd6c5634300d1de7b3df1a294dfd649
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-01-03 15:31:27 -0500

    comments

--- WebService/DB/DatabaseSettings.cs
+++ WebService/DB/DatabaseSettings.cs
@@ -2,8 +2,13 @@ namespace WebService.DB
 {
     public class DatabaseSettings
     {
+        // name or network address of the instance of SQL Server to connect to
         public string DataSource { get; set; }
+
+        // name of the database associated with the connection
         public string InitialCatalog { get; set; }
+
+        // time (in seconds) to wait for a connection to the server before terminating the attempt and generating an error
         public int Timeout { get; set; }
     }
 }
