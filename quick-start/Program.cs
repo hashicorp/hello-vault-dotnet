42bfbe0a44d0046da7892e6f19b1132c26e4832a
Valerie Conklin <val@hashicorp.com>
2022-02-11 11:18:16 -0800

    Quickstart code

---
+++ quick-start/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
+
+namespace DeveloperQuickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Authenticate
+            IAuthMethodInfo authMethod = new TokenAuthMethodInfo(vaultToken: "dev-only-token");
+
+            VaultClientSettings vaultClientSettings = new VaultClientSettings("http://127.0.0.1:8200", authMethod);
+            IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+            // Write a secret
+            var secretData = new Dictionary<string, object> { { "password", "Hashi123" } };
+            vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync(
+                path: "/my-secret-password",
+                data: secretData,
+                mountPoint: "secret"
+            ).Wait();
+
+            Console.WriteLine("Secret written successfully.");
+
+            // Read a secret
+            Secret<SecretData> secret = vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
+                path: "/my-secret-password",
+                mountPoint: "secret"
+            ).Result;
+
+            var password = secret.Data.Data["password"];
+
+            if (password.ToString() != "Hashi123")
+            {
+                throw new System.Exception("Unexpected password");
+            }
+
+            Console.WriteLine("Access granted!");
+        }
+    }
+}
--- quick-start/README.md
+++ quick-start/README.md
@@ -1,1 +1,16 @@
 # Quick Start
+
+This is a quick example of how to retrieve your first secret from HashiCorp
+[Vault](https://www.vaultproject.io/), using the community-maintained
+client library [VaultSharp](https://github.com/rajanadar/VaultSharp).
+
+## How To Use
+
+Run the script `./run.sh` to set up a Vault dev server and run the example code.
+
+You just wrote and read your first Vault secret!
+
+## Next Steps
+
+For a more realistic example of a runnable web app connecting to Vault using the
+AppRole authentication method, check out the [sample app](../sample-app).
